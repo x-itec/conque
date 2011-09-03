1f87168c6a1e018be792a00e0256468906b64c5a
nicoraffo <nicoraffo@18cfea20-95ec-11de-9f73-7911e0306dd8>
2011-09-03 05:37:20 +0000

    unused

--- util/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-sed -i 's#^.*logging.*$##' autoload/conque_term.vim
-sed -i 's#^.*debug.*$##' autoload/conque_term.vim
-sed -i 's#^.*LOG_FILENAME.*$##' autoload/conque_term.vim
-
-rm -r tests
-
